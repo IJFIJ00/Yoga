4ca95717f8fef856fe0b39d8452a377ac08c04dc
IJFIJ00 <74531966+IJFIJ00@users.noreply.github.com>
2020-12-09 12:53:09 +0300

    Add files via upload

---
+++ Yoga/Yoga/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  Yoga
+//
+//  Created by Дмитрий on 08.12.2020.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var title: UILabel!
+   
+}
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
